ca8a27b3bd8840fa81ad15b9d637b516ea84f5b7
Lucas Viana <lucasvfv@gmail.com>
2025-09-16 12:42:32 -0300

    Refatoração do código e criação do menu interativo

---
+++ OcultDay/OcultDay.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OcultDay
+{
+    public class OcultDayMain
+    {
+        public OcultDayMain() {
+            
+        }
+        public void Menu()
+        {
+            int valor;
+            do
+            {
+                Console.WriteLine(
+                    "Digite o id de uma das opções que deseja iniciar: \n" +
+                    "[1] Adicionar partcipante\n" +
+                    "[2] Remover participante\n" +
+                    "[3] Listar participantes\n" +
+                    "[4] Realizar sorteio\n" +
+                    "[0] Sair"
+                    );
+                valor = int.Parse(Console.ReadLine()); 
+                Escolha(valor);
+            } while (valor != 0);
+        }
+
+        public void Escolha(int valor)
+        {
+            switch (valor)
+            {
+                case 1:
+                    //AdicionarParticipante();
+                    break;
+                case 2:
+                    //RemoverParticipante();
+                    break;
+                case 3:
+                    //ListarParticipantes();
+                    break;
+                case 4:
+                    //RealizarSorteio();
+                    break;
+                case 0:
+                    Console.WriteLine("Saindo...");
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida. Tente novamente.");
+                    break;
+            }
+        }
+    }
+}
